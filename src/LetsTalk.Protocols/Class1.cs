2b4ad7971b402d9c50b0f4309bd81e1cd95ffe6f
Karl O'Brien <karlobrien@gmail.com>
2020-08-22 21:05:57 +0100

    simple example of length protocol working

---
+++ src/LetsTalk.Protocols/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Buffers;
+using System.Buffers.Binary;
+
+namespace LetsTalk.Protocols
+{
+    public class LengthProtocol : IMessageProtocol
+    {
+        const int HeaderSize = 4;
+        public void WriteMessage(Message message, IBufferWriter<byte> output)
+        {
+            var header = output.GetSpan(HeaderSize);
+            BinaryPrimitives.WriteUInt32BigEndian(header, (uint)message.Payload.Length);
+            output.Advance(HeaderSize);
+
+            foreach (var msg in message.Payload)
+                output.Write(msg.Span);
+        }
+
+        public bool TryParseMessage(in ReadOnlySequence<byte> input, ref SequencePosition consumed, ref SequencePosition examined, out Message message)
+        {
+            var reader = new SequenceReader<byte>(input);
+
+            if (input.Length < HeaderSize)
+            {
+                message = default;
+                return false;
+            }
+
+            //https://github.com/grpc/grpc-dotnet/blob/6504c26be7ff763f6367daf1a43b8c01eefc3e7a/src/Grpc.AspNetCore.Server/Internal/PipeExtensions.cs#L158-L184
+            int length = 0;
+            if (input.First.Length >= HeaderSize)
+            {
+                var header = input.First.Span.Slice(0, HeaderSize);
+                length = BinaryPrimitives.ReadInt32BigEndian(header);
+            }
+            else
+            {
+                Span<byte> header = stackalloc byte[HeaderSize];
+                input.Slice(0, HeaderSize).CopyTo(header);
+                length = BinaryPrimitives.ReadInt32BigEndian(header);
+            }
+
+            //check here if we have enough to read the message otherwise try and read more data
+            if (input.Length < HeaderSize + length)
+            {
+                message = default;
+                return false;
+            }
+
+            //could have used input.slice(HeaderSize + length) here
+            var t = input.Slice(HeaderSize, length);
+            //TODO: Do we go from a pool of preallocated messages here?
+            message = new Message(t);
+
+            consumed = t.End;
+            examined = consumed;
+
+            return true;
+        }
+    }
+
+    public interface IMessageProtocol
+    {
+        bool TryParseMessage(in ReadOnlySequence<byte> input, ref SequencePosition consumed, ref SequencePosition examined, out Message message);
+        void WriteMessage(Message message, IBufferWriter<byte> output);
+    }
+
+    public struct Message
+    {
+        public Message(byte[] payload) : this(new ReadOnlySequence<byte>(payload)) { }
+
+        public Message(ReadOnlySequence<byte> payload)
+        {
+            Payload = payload;
+        }
+
+        public ReadOnlySequence<byte> Payload { get; }
+    }
+}
--- src/LetsTalk.Server/Program.cs
+++ src/LetsTalk.Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LetsTalk.Protocols;
+using System;
 using System.Buffers;
 using System.IO.Pipelines;
 using System.Net;
@@ -20,9 +21,55 @@ namespace LetsTalk.Server
             while (true)
             {
                 var socket = await listenSocket.AcceptAsync();
-                _ = ProcessLinesAsync(socket);
+                _ = ReadPipeAsync(socket);
+                //_ = ProcessLinesAsync(socket);
             }
         }
+
+        private static async Task ReadPipeAsync(Socket socket)
+        {
+            Console.WriteLine($"[{socket.RemoteEndPoint}]: connected");
+
+            // Create a PipeReader over the network stream
+            var stream = new NetworkStream(socket);
+            var reader = PipeReader.Create(stream);
+
+            IMessageProtocol messageProtocol = new LengthProtocol();
+            while (true)
+            {
+                ReadResult result = await reader.ReadAsync();
+                ReadOnlySequence<byte> buffer = result.Buffer;
+
+                SequencePosition consumed = buffer.Start;
+                SequencePosition examined = buffer.Start;
+                if (messageProtocol.TryParseMessage(buffer, ref consumed, ref examined, out var msg))
+                {
+                    ProcessMessage(msg);
+                }
+
+                if (result.IsCompleted)
+                    break;
+
+                reader.AdvanceTo(consumed);
+            }
+
+        }
+
+        private static void ProcessMessage(Message msg)
+        {
+            var readResult = Encoding.UTF8.GetString(msg.Payload.ToArray());
+            Console.WriteLine(readResult);
+        }
+
+
+
+
+
+
+
+
+
+
 
         private static async Task ProcessLinesAsync(Socket socket)
         {
