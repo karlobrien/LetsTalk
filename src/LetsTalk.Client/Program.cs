68d38b8af02e6a6bebce47233a4e3191bc2a1193
Karl O'Brien <karlobrien@gmail.com>
2020-08-22 16:51:24 +0100

    setup

---
+++ src/LetsTalk.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace LetsTalk.Client
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Connecting to port 8087");
+
+            clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, 8087));
+            var stream = new NetworkStream(clientSocket);
+
+            await Console.OpenStandardInput().CopyToAsync(stream);
+        }
+    }
+}
